c229e9de1c82c88eaa6ecebd46c68822d9a49b9f
0ung <gupo941020@naver.com>
2025-11-17 10:27:11 +0900

    [init] project

--- src/main/java/com/bitmax/ojt/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bitmax.ojt.repository;
-
-import com.bitmax.ojt.entitiy.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board,Long> {
-}
